eef86a39d3cfb4febea95f10e5a53b418c8700d2
KLEBERSONKJS <klebbss4@gmail.com>
2024-12-29 15:23:27 -0300

    Initial commit

---
+++ src/main/java/com/ads/pwb/sistemaProgramacaoWeb/SistemaProgramacaoWebApplication.java
@@ -0,0 +1,13 @@
+package com.ads.pwb.sistemaProgramacaoWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaProgramacaoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaProgramacaoWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ads/pwb/sistemaProgramacaoWeb/SistemaProgramacaoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ads.pwb.sistemaProgramacaoWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaProgramacaoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
